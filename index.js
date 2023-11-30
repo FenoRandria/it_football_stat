78fb8a4949cd224750f5c77c94e76b2398883992
FenoRandria <fenorandriamampionona2@gmail.com>
2023-11-30 03:17:53 -0500

    configuration general

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const routesFootball = require("./routes/routesFootball");
+app.use("/football", routesFootball);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
