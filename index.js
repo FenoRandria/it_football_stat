fe6e0e8a70ee3df19e4784e54c17009c1c470fde
FenoRandria <fenorandriamampionona2@gmail.com>
2023-11-30 04:10:35 -0500

    commit by feno

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
 
-const routesFootball = require("./routes/routesFootball");
+const routesFootball = require("./route/routesFootball");
 app.use("/football", routesFootball);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
