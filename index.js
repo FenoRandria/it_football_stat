850c6da11b6c22196ca9296761c06bce96b9d1b9
FenoRandria <fenorandriamampionona2@gmail.com>
2023-11-30 13:19:37 -0500

    correction connection railway

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 4321;
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
